4fb193b2b6332d1c6e71bb43de786e1fcb873d71
Charlie Oster <oster.charlie@yahoo.com>
2019-09-06 02:26:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,8 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- sort(array).last
- sort(array).first
+ array.sort(array).last
+ array.sort(array).first
 end
 
 def using_reverse(array)
